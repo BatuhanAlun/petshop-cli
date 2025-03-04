88577ab809e13c7a01c4ed594bc7242d0e6e4dc2
BatuhanAlun <150372974+BatuhanAlun@users.noreply.github.com>
2025-03-04 23:29:06 +0300

    Interface

--- domain/animal.go
+++ domain/animal.go
@@ -1,16 +1,5 @@
 package domain
 
-type IAnimal interface {
-	RegisterAnimal(name, animalType string) error
-	DeleteAnimal(deleteId int) error
-	GetAnimalInfo(id int) (domain.Animal, error)
-	UpdateAnimal(updateId, newOwnerId int, newName, newType, newNickname string) error
-	GetAnimals() ([]domain.Animal, error)
-	GetNotAdoptedAnimals() ([]domain.Animal, error)
-	GetOwnedAnimals(ownerId int) ([]domain.Animal, error)
-	ChangeAnimalNickname(adoptId int, newNick string) error
-}
-
 type Animal struct {
 	ID       int
 	Name     string
